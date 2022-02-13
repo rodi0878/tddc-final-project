5a388b47f2889b0813d9dea6ffbbd4f668cbc21f
Roman Diviš <divisak@gmail.com>
2022-02-13 17:48:21 +0100

    Initial commit

---
+++ src/test/java/cz/upce/tddc/finalproject/FinalprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.upce.tddc.finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
